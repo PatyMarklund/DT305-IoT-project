b84691c78821f3f5c23b7d215a24cdd4799b4ada
Patricia Marklund <antunes@student.chalmers.se>
2023-06-28 00:06:25 +0200

    Add libraries to LCD and code to make it work

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ import utime as time
 from dht import DHT11
 # from machine import DHT11
 import wifi
+from machine import I2C, Pin
+from pico_i2c_lcd import I2cLcd
+i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=400000)
+
+I2C_ADDR = i2c.scan()[0]
+lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
+while True:
+  print(I2C_ADDR, "| Hex:",hex(I2C_ADDR))
+  print()
+  lcd.move_to(0,0)
+  lcd.putstr("I2CAddress:"+hex(I2C_ADDR)+"\n")
+  lcd.move_to(0,1)
+  lcd.putstr("Nice weather!")
+  lcd.backlight_on()
 
 # The GPIO number is 13 which is equal to the pin number 17
 """
