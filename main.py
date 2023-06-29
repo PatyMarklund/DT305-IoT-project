89d7b3e4a3743c1fe9669b45467880272361845b
Patricia Marklund <antunes@student.chalmers.se>
2023-06-30 00:16:51 +0200

    Fix wether message code

--- main.py
+++ main.py
@@ -11,39 +11,11 @@ from dht import DHT11
 import wifi
 from machine import I2C, Pin
 from pico_i2c_lcd import I2cLcd
-i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=400000)
-
-# I2C_ADDR = i2c.scan()[0]
-# lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
-# while True:
-#   print(I2C_ADDR, "| Hex:",hex(I2C_ADDR))
-#   print()
-#   lcd.move_to(0,0)
-#   lcd.putstr("I2CAddress:"+hex(I2C_ADDR)+"\n")
-#   lcd.move_to(0,1)
-#   lcd.putstr("Nice weather!")
-#   lcd.backlight_on()
-
-# The GPIO number is 13 which is equal to the pin number 17
-
-# pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
-# sensor = DHT11(pin)
-
-# while True:
-#     time.sleep(2)
-#     try:
-#         t = sensor.temperature
-#         time.sleep(2)
-#         h = sensor.humidity
-#     except:
-#         print("An exception occurred")  
-#         continue  
-#     print("Temperature testing: {}".format(sensor.temperature))
-#     print("Humidity: {}".format(sensor.humidity))
 
 
 # BEGIN SETTINGS
-RANDOMS_INTERVAL = 1000   # milliseconds
+i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=400000)
+RANDOMS_INTERVAL = 60   # milliseconds
 last_random_sent_ticks = 0  # milliseconds
 led = Pin("LED", Pin.OUT)   # led pin initialization for Raspberry Pi Pico W
 
@@ -51,7 +23,7 @@ led = Pin("LED", Pin.OUT)   # led pin initialization for Raspberry Pi Pico W
 AIO_SERVER = "io.adafruit.com"
 AIO_PORT = 1883
 AIO_USER = "Paty_Marklund"
-AIO_KEY = "aio_yTAH86DEYIOID7IgCHUthz3JOHVq"
+AIO_KEY = "aio_YreN52bFpnin2Ctj4BWmRA9Gbhm0"
 AIO_CLIENT_ID = ubinascii.hexlify(machine.unique_id()) 
 AIO_LIGHTS_FEED = "Paty_Marklund/feeds/lights"
 AIO_TEMP_FEED = "Paty_Marklund/feeds/temperature"
@@ -76,32 +48,14 @@ def sub_cb(topic, msg):          # sub_cb means "callback subroutine"
 def random_integer(upper_bound):
     return random.getrandbits(32) % upper_bound
 
-"""
-# Function to publish random number to Adafruit IO MQTT server at fixed interval
-def send_random():
-    global last_random_sent_ticks
-    global RANDOMS_INTERVAL
-
-    if ((time.ticks_ms() - last_random_sent_ticks) < RANDOMS_INTERVAL):
-        return; # Too soon since last one sent.
-
-    #some_number = random_integer(100)
-    print("Publishing: {0} to {1} ... ".format(some_number, AIO_RANDOMS_FEED), end='')
-    try:
-        client.publish(topic=AIO_RANDOMS_FEED, msg=str(some_number))
-        print("DONE")
-    except Exception as e:
-        print("FAILED")
-    finally:
-        last_random_sent_ticks = time.ticks_ms()
-"""
-
 def get_temperature():
     pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
     # sensor = DHT11(pin)
     sensor = DHT11(machine.Pin(28))
     global last_random_sent_ticks
     global RANDOMS_INTERVAL
+    prev_temp = None
+    prev_humid = None
     
     if ((time.ticks_ms() - last_random_sent_ticks) < RANDOMS_INTERVAL):
         return; # Too soon since last one sent.
@@ -110,69 +64,77 @@ def get_temperature():
         time.sleep(2)
         try:
             temp = sensor.temperature
+            #prev_temp = temp
             time.sleep(2)
             humid = sensor.humidity
+            #prev_humid = humid
         except:
             print("An exception occurred")  
             continue  
-        message = weather_report(temp, humid)
+        
+        message_1, message_2 = weather_report(temp, humid)
+        publish_message = message_1 + " / " + message_2
+        
+        if (prev_humid is None or prev_temp is None) or (temp != prev_temp and humid != prev_humid):
+            prev_temp = temp
+            prev_humid = humid
+            display_message(message_1, message_2)
             
         print("Publishing: {0} to {1} ... ".format(temp, AIO_TEMP_FEED), end='')
         print("Publishing: {0} to {1} ... ".format(humid, AIO_HUMID_FEED), end='')
-        print("Publishing: {0} to {1} ... ".format(message, AIO_MESSAGE_FEED), end='')
+        print("Publishing: {0} to {1} ... ".format(publish_message, AIO_MESSAGE_FEED), end='')
         
         try:
             client.publish(topic=AIO_TEMP_FEED, msg=str(temp))
             client.publish(topic=AIO_HUMID_FEED, msg=str(humid))
-            client.publish(topic=AIO_MESSAGE_FEED, msg=str(message))
+            client.publish(topic=AIO_MESSAGE_FEED, msg=str(publish_message))
             print("DONE")
         except Exception as e:
             print("FAILED")
         finally:
             last_random_sent_ticks = time.ticks_ms()
-        #return temp, humid
             
-def weather_report(temperature, humidity):
+def weather_report(temp, humidity):
+    temperature = int(temp)
     message_1 = " "
     message_2 = " "
-    if temperature > 30:
-        message_1 = str(temperature)+"Too hot!"
+    if temperature > 30 and humidity < 70:
+        message_1 = str(temperature)+"C Too hot!"
         message_2 = "Shorts & flops!"
-    elif temperature > 25:
-        message_1 = str(temperature)+"Warm weather"
+    elif temperature > 25 and humidity < 70:
+        message_1 = str(temperature)+"C Warm weather"
         message_2 = "T-shirt & hat"
-    elif temperature > 20:
-        message_1 = str(temperature)+"Nice weather"
+    elif temperature > 20 and humidity < 70:
+        message_1 = str(temperature)+"C Nice weather"
         message_2 = "Light jacket"
-    elif temperature > 10:
-        message_1 = str(temperature)+"Bit chill"
+    elif temperature > 10 and humidity < 70:
+        message_1 = str(temperature)+"C Bit chill"
         message_2 = "Jacket"
     elif temperature > 0 and humidity > 70:
-        message_1 = str(temperature)+"Rainy day"
+        message_1 = str(temperature)+"C Rainy day"
         message_2 = "Rain cloths"
-    elif temperature > 0:
-        message_1 = str(temperature)+"Too cold"
+    elif temperature > 0 and humidity < 70:
+        message_1 = str(temperature)+"C Too cold"
         message_2 = "Overalls"
     elif temperature < 0 and humidity > 70:
-        message_1 = str(temperature)+"Snow day"
+        message_1 = str(temperature)+"C Snow day"
         message_2 = "Put everything"
     else:
-        message_1 = str(temperature)+"Too cold"
+        message_1 = str(temperature)+"C Too cold"
         message_2 = "Overalls"
         
-    return message_1
+    display_message(message_1, message_2)
+        
+    return message_1, message_2
     
-def display_message(temperature, message):
+def display_message(message_1, message_2):
     I2C_ADDR = i2c.scan()[0]
     lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
-    while True:
-        print(I2C_ADDR, "| Hex:",hex(I2C_ADDR))
-        print()
-        lcd.move_to(0,0)
-        lcd.putstr("I2CAddress:"+hex(I2C_ADDR)+"\n")
-        lcd.move_to(0,1)
-        lcd.putstr("Nice weather!")
-        lcd.backlight_on()
+    print()
+    lcd.move_to(0,0)
+    lcd.putstr(message_1+"\n")
+    lcd.move_to(0,1)
+    lcd.putstr(message_2)
 
 # Try WiFi Connection
 try:
@@ -190,14 +152,10 @@ client.subscribe(AIO_LIGHTS_FEED)
 print("Connected to %s, subscribed to %s topic" % (AIO_SERVER, AIO_LIGHTS_FEED))
 
 
-try:                      # Code between try: and finally: may cause an error
-                          # so ensure the client disconnects the server if
-                          # that happens.
+try:                      
     while 1:              # Repeat this loop forever
         client.check_msg()# Action a message if one is received. Non-blocking.
         get_temperature()
-        #weather_report(temp, humid)
-        #send_random()     # Send a random number to Adafruit IO if it's time.
 finally:                  # If an exception is thrown ...
     client.disconnect()   # ... disconnect the client and clean up.
     client = None
@@ -207,8 +165,6 @@ finally:                  # If an exception is thrown ...
     """
     * Code part:
     
-    - Create code to calculate the temperature and humidity to send a message of the weather
-        (either the code will be done on the board or on the dashboard)
     - Send message from dashboard to the display for the weather condition
     - Separate methods out of the main()
     - Clean the code
